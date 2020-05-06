10714056e59f482c2c2d50f32b07edd1101a8886
arnaudebonis <39151496+arnaudebonis@users.noreply.github.com>
2020-05-07 00:19:55 +0200

    Initial commit

---
+++ Assets/extOSC/Scripts/Project/ColorChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public GameObject reference;
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, -reference.transform.position.x/100, 0);
+    }
+}
